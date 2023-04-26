301d838cd9dbd1be212cf121301acafad051ac85
ltroiani <lucio.troiani@rci.rogers.com>
2023-04-26 14:44:30 -0400

    Code from codepen

---
+++ threestars/dist/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+        $.ajax({
+          type: "GET",
+          url: "https://statsapi.web.nhl.com/api/v1/game/2022020806/feed/live",
+          success: function(data) {
+            var gameDate = data.gameData.datetime.dateTime;
+            $("#gameDate").html(gameDate);
+
+            var homeTeam = data.gameData.teams.home.teamName;
+            var awayTeam = data.gameData.teams.away.teamName;
+            $("#gameTeams").html(awayTeam + " @ " + homeTeam);
+            $("#awayTeam").html(awayTeam);
+                        
+            var firstStar = data.liveData.decisions.firstStar.fullName;
+            $("#firstStar").html(firstStar);
+
+            var secondStar = data.liveData.decisions.secondStar.fullName;
+            $("#secondStar").html(secondStar);
+
+            var thirdStar = data.liveData.decisions.thirdStar.fullName;
+            $("#thirdStar").html(thirdStar);
+          }
+        });
+      });
